9c02f981b3397c6687036e0d90cdbb78bbda47a5
Denis Rodrigues <89110188+guerreiro-denis@users.noreply.github.com>
2023-05-16 20:35:22 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -1,38 +1,45 @@
 function validaOChute(chute) {
     const numero = +chute;
     //além de cria a const a soma faz com que o type seja int.
-    if (chuteNaoNumeral(numero)) {
-        //confere Number.isNaN confere se o parâmetro é ou não número, true significa q é Nan.
-        elementoChute.innerHTML = `
-        <div>Você disse:</div>
-        <span class="box">${chute}</span>
-        <div>Valor inválido: tente chutar um número natural.</div>
-    `
-    }
     
-    if (chuteForaDoRange(numero)) {
-        elementoChute.innerHTML = `
-        <div>Você disse:</div>
-        <span class="box">${chute}</span>
-        Valor inválido: O número secreto estar entre ${menorValor} e ${maiorValor}.
-        `
-    }
-
-    if (numero === numeroSecreto) {
+    if (comandoFimDeJogo(numero)) {
         document.body.innerHTML = `
-        <h2>Parabéns você acertou!</h2>
-        <h3>O número secreto era ${numeroSecreto}</h3>
+            <h2>Fim do jogo</h2>
+            <h3>O número secreto era ${numeroSecreto}, gostaria de jogar novamente?</h3>
+    
+            <button id="botao-ng" class="botao-ng">Jogar novamente</button>
+            `
+    } if (chuteNaoNumeral(numero) && chute !== "Fim de jogo" && chute !== "fim de jogo" && chute !== "Fim do jogo" && chute !== "fim do jogo") {
+        //Number.isNaN confere se o parâmetro é ou não número, true significa q é NaN.
+        //o if sendo true ele faz a condicional;
+        elementoChute.innerHTML = `
+            <div>Você disse:</div>
+            <span class="box">${chute}</span>
+            <div>Valor inválido: tente chutar um número natural.</div>
+        `
+        console.log(numero);
 
-        <button id="botao-ng" class="botao-ng">Jogar novamente</button>
+    } if (chuteForaDoRange(numero)) {
+        elementoChute.innerHTML = `
+            <div>Você disse:</div>
+            <span class="box">${chute}</span>
+            Valor inválido: O número secreto está entre ${menorValor} e ${maiorValor}.
+        `
+    } if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Parabéns você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="botao-ng" class="botao-ng">Jogar novamente</button>
         `
     } else if (numero < numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
         `;
 
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
         `;
     }
 }
@@ -40,6 +47,10 @@ function validaOChute(chute) {
 function chuteNaoNumeral(numero) {
     return Number.isNaN(numero);
     //confere Number.isNaN confere se o parâmetro é ou não número, true significa q é Nan.
+}
+
+function comandoFimDeJogo(numero) {
+    return chute === "Fim de jogo" || chute === "fim de jogo" || chute === "Fim do jogo" || chute === "fim do jogo";
 }
 
 function chuteForaDoRange(numero) {
